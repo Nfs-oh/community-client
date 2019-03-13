0cb0fb2e3a65f50629ff3144f911d6289cb7d85e
clh <978702126@qq.com>
2019-03-13 23:12:25 +0800

    添加wangedit

---
+++ src/components/wangeidtor/Wangeditor.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import E from 'wangeditor';
+import { Button } from 'antd'
+
+class Wangeditor extends Component {
+	constructor() {
+		super();
+		this.state = {
+			editContent: ""
+		}
+	}
+	componentDidMount() {
+		let editorElem = this.refs.editorElem;
+		const editor = new E(editorElem);
+		editor.customConfig.onchange = html => {
+	      this.setState({
+	        editorContent: html
+	      })
+	    }
+    	editor.create()
+	}
+	handleSave = () => {
+		let { receiveArticle } = this.props;
+		let { editContent } = this.state;
+		receiveArticle(editContent);
+	}
+  	render() {
+   	 	return (
+      		<div className="wangeditor">
+          		<div ref="editorElem" className="wangeditor-box"></div>
+          		<div className="save-edit">
+          			<Button onClick={this.handleSave}>保存</Button>
+          		</div>
+      		</div>
+    	);
+  	}
+}
+
+export default Wangeditor;
