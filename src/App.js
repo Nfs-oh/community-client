b78342741a750df394b8c092f6e7cbd4394b0651
ken <lianghui@taotaosou.com>
2019-03-15 14:19:17 +0800

    修改：配置antd样式按需加载

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import {Button} from 'antd';
 class App extends Component {
   render() {
     return (
       <div className="App">
-          
+          <Button type="primary">53543</Button>
       </div>
     );
   }
