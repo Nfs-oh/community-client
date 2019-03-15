41b433fa32dd991af6971855b2cef365092f9136
ken <lianghui@taotaosou.com>
2019-03-15 16:43:08 +0800

    调整目录结构

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import {Button} from 'antd';
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Button type="primary">53543</Button>
-      </div>
-    );
-  }
+	render() {
+	    return (
+	      	<div className="App">
+	      	</div>
+	    );
+	}
 }
 
 export default App;
---
+++ src/views/login/Regist.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Register extends Component {
+  	render() {
+    	return (
+      		<div className="regist">
+      		/div>
+    	);
+  	}
+}
+
+export default Register;
