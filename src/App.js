f970445650230ea7ae79a9857679ec76309db55c
ken <lianghui@taotaosou.com>
2019-03-19 18:26:43 +0800

    添加：增加编辑器

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import Edit from './views/edit/Eidt.js';
 class App extends Component {
 	render() {
 	    return (
 	      	<div className="App">
+			  <Edit />
 	      	</div>
 	    );
 	}
